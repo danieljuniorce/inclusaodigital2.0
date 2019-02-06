9eca7a273b4218e39162b1c5da49d49f941ddcd6
danieljuniorce <danieljuniorce@hotmail.com>
2019-02-06 01:51:28 -0200

    V1.2.6

--- config.php
+++ config.php
@@ -8,26 +8,8 @@
 
 require 'environment.php';
 
-global $config;
-$config = array();
 
-//Nome do Site
-$config['NomeProject'] = 'Inclusão Digital';
 
-//Configuração e conexão com Banco de dados;
-global $db;
-$db = array();
-if (ENVIRONMENT === 'develepment') {
-    $db['user'] = 'root';
-    $db['password'] = '';
-    $db['host'] = 'localhost';
-    $db['name'] = 'inclusaodigital';
-} else {
-    $db['user'] = 'root';
-    $db['password'] = '';
-    $db['host'] = 'localhost';
-    $db['name'] = 'inclusaodigital';
-}
 
 
 
